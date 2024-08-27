4e32268e61446cb26a6ef8dc2c7f515f8f65b625
remylouisew <remylouisew@gmail.com>
2024-08-27 21:50:29 +0000

    flex template

--- dataflowGPU_test.sh
+++ dataflowGPU_test.sh
@@ -4,7 +4,7 @@ docker image tag remytest us-central1-docker.pkg.dev/cool-machine-learning/axlea
 docker image push  us-central1-docker.pkg.dev/cool-machine-learning/axlearn/axlearn-dataflow:remywtest
 
 
-DOCKER_REPO=us-central1-docker.pkg.dev/cool-machine-learning/axlearn
+DOCKER_REPO=us-central1-docker.pkg.dev/cool-machine-learning/axlearn/axlearn-dataflow
 DOCKER_IMAGE=axlearn-dataflow
 
 axlearn gcp dataflow start \
@@ -48,3 +48,42 @@ axlearn gcp dataflow start \
 --name=remyw-dataflow1 \
     #--bundler_spec=base_image=apache/beam_python3.10_sdk:2.52.0 \
     #--bundler_spec=target=dataflow \
+
+#try with flat formatting
+#and hardocded dataflow_service_options
+axlearn gcp dataflow start --bundler_spec=dockerfile=Dockerfile --bundler_spec=repo=us-central1-docker.pkg.dev/cool-machine-learning/axlearn --bundler_spec=image=axlearn-dataflow --bundler_spec=target=dataflow --bundler_spec=allow_dirty=True --dataflow_spec=runner=DataflowRunner -- "python3 -m apache_beam.examples.wordcount --input=gs://dataflow-samples/shakespeare/kinglear.txt --output=gs://dataflow-test-bucket0/rwtest/output/ "
+
+# from Zhiyun Lu
+
+DOCKER_REPO=us-docker.pkg.dev/cybertron-gcp-island-test-0rxn/applejax
+DOCKER_IMAGE=ajax-dataflow
+DOCKER_TAG=generate_preprocessed_example_${USER}2
+PERM_BUCKET="ttl-30d-us-central2-0rxn"
+
+DATA_DIR="gs://permanent-us-central2-0rxn/tensorflow_datasets"
+OUTPUT_DATA_DIR="gs://ttl-30d-us-central2-0rxn/tensorflow_datasets/${USER}/"
+CONFIG_MODULE=mixture_general_lm
+CONFIG_NAME=v1-85m
+OUTPUT_DATASET=mixture_general_lm_4t2
+NUM_SHARDS=10
+MAX_EXAMPLES_PER_SHARD=100
+axlearn gcp dataflow start \
+    --name=preprocess-${USER}2 \
+    --dataflow_spec=runner=DataflowRunner \
+    --dataflow_spec=disk_size_gb=64 \
+    --dataflow_spec=resource_hints=min_ram=64GB \
+    --dataflow_spec=number_of_worker_harness_threads=1 \
+    --vm_type=n1-standard-32 \
+    --bundler_spec=platform=linux/amd64 \
+    --bundler_spec=dockerfile=ajax/tools/Dockerfile.dataflow \
+    --bundler_spec=DATAFLOW_PYTHON_VERSION=3.9 \
+    --bundler_spec=image=${DOCKER_IMAGE} \
+    --bundler_spec=repo=${DOCKER_REPO} \
+    -- "'
+  python3 -m ajax.tools.generate_deterministic_input_examples \
+    --config_module=${CONFIG_MODULE} --config=${CONFIG_NAME} \
+    --input_data_dir=${DATA_DIR} \
+    --output_data_dir=${OUTPUT_DATA_DIR} \
+    --output_dataset=${OUTPUT_DATASET} \
+    --num_shards=${NUM_SHARDS} \
+    --max_examples_per_shard=${MAX_EXAMPLES_PER_SHARD}'"
